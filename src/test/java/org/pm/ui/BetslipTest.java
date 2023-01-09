91076499478f255b27c5c0dbf907236d3801d58b
Viktoriya Aparina <viktoriya.aparina.aqa@gmail.com>
2023-01-09 11:28:43 +0300

    viktoriya_aparyna_ui: refactoring pom.xml

--- src/test/java/org/pm/ui/BetslipTest.java
+++ src/test/java/org/pm/ui/BetslipTest.java
@@ -1,20 +1,20 @@
 package org.pm.ui;
 
+import io.qameta.allure.Description;
 import org.pm.ui.pages.Bet;
 import org.pm.ui.pages.HomePage;
 import org.testng.annotations.Test;
 
 public class BetslipTest extends BaseTest {
-
-    @Test
+    
+    @Description("UI: Place a single bet using proposed bet amount")
+    @Test(groups = "Aparina UI tests")
     public void addSingleBetFromProposedBetsTest() {
-        HomePage.open().clickOnLoginButton().chooseLoginTypeById()
-                .loginWithTestCredentialId()
-                .selectSport("soccer")
+        new HomePage().selectSport("soccer")
                 .selectEventAndOutcome(1, Bet.P1)
                 .chooseProposedAmountForBet()
                 .clickPlaceBetButtonOnBetslip();
-        //assertThat(new BetslipPage().getAlert()).as("Single bet was created successfully! " +
-        //    "The text in alert is differ from expected").isEqualTo("Bet accepted");*/
+        /*assertThat(new BetslipPage().getAlert()).as("Single bet was created successfully! " +
+          "The text in alert is differ from expected").isEqualTo("Bet accepted");*/
     }
 }
