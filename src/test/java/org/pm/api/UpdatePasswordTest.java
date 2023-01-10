867a13e68735bb8c9edfbbf4a61c22a0361ab105
Valeriia Riabtseva <valeriiariabtseva@gmail.com>
2023-01-10 22:10:37 +0200

    updated API test

--- src/test/java/org/pm/api/UpdatePasswordTest.java
+++ src/test/java/org/pm/api/UpdatePasswordTest.java
@@ -1,52 +1,31 @@
 package org.pm.api;
 
-import static org.hamcrest.Matchers.notNullValue;
-
-import lombok.var;
+import api.clients.UpdatePasswordApiClient;
+import api.dto.UpdatePasswordRequest;
+import api.dto.UserRegistrationRequest;
+import api.dto.response.RegistrationResponse;
+import api.providers.UserRegistrationProvider;
+import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
-
-import org.pm.api.controllers.UpdatePasswordController;
-import org.pm.api.model.AccessToken;
-import org.pm.api.model.UpdatePasswordRequest;
+import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class UpdatePasswordTest extends BaseTest {
+public class UpdatePasswordTest extends BaseTest{
 
-    UpdatePasswordController updatePasswordController = new UpdatePasswordController();
+    UpdatePasswordApiClient updatePasswordApiClient = new UpdatePasswordApiClient();
 
     @Test
-    public void updatePasswordTest() {
+    public void updatePasswordTest(){
+        UserRegistrationRequest newUser = new UserRegistrationProvider().getNewUser();
+        RegistrationResponse registrationResponse = registrationApiClient.postUser(newUser, HttpStatus.SC_OK);
+
         String generatedString = RandomStringUtils.randomNumeric(3);
-        var phone = "+38011065" + generatedString;
-        var email = "test_" + generatedString + "@gmail.com";
-        var newPassword = "Qwerty!" + generatedString;
+        String newPassword = "Qwerty!" + generatedString;
 
-        var userForRegistration = buildUserForRegistration(
-                formName, phone, email, password, defaultCurrency, selectedLanguage, isPlayerAgree);
-
-        var actualRegistrationResponse =
-                registrationController.registrationNewUser(userForRegistration);
-
-        Assert.assertEquals(actualRegistrationResponse.extract().statusCode(), 200);
-        actualRegistrationResponse.body("token", notNullValue());
-
-        logger.info("The user registered with password " + password);
-
-        var token = actualRegistrationResponse.extract().as(AccessToken.class).getToken();
-
-        var requestUpdate = buildUserForUpdate(password, newPassword);
-
-        var actualUpdateResponse = updatePasswordController.updatePassword(requestUpdate, token);
-
-        Assert.assertEquals(actualUpdateResponse.extract().statusCode(), 200);
-        logger.info("Password successfully changed to " + newPassword);
-    }
-
-    private UpdatePasswordRequest buildUserForUpdate(String oldPassword, String newPassword) {
-        return UpdatePasswordRequest.builder()
-                .oldPassword(oldPassword)
-                .newPassword(newPassword)
-                .build();
+        UpdatePasswordRequest updatePasswordRequest = new UpdatePasswordRequest(newUser.getPassword(), newPassword);
+        Response actualResponse = updatePasswordApiClient
+                .postUpdatePassword(updatePasswordRequest, registrationResponse.getToken());
+        Assert.assertEquals(actualResponse.then().extract().statusCode(), HttpStatus.SC_OK);
     }
 }
